dab17ba55d72a9dff18135f77226119543fccff0
jikefan <1791504346@qq.com>
2024-12-02 18:15:32 +0800

    Finished TCP Transport and TCPPeer Created.

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -6,6 +6,29 @@ import (
 	"sync"
 )
 
+// TCPPeer represents the remote node over a TCP established connection.
+//
+// TCPPeer 表示在 TCP 建立的连接上的远程节点。
+type TCPPeer struct {
+	conn net.Conn
+
+	// if we dial and retrieve a conn -> outbound == true
+	//
+	// if we accept and retrieve a conn -> outbound == false
+	//
+	// 如果我们拨号并获取到一个连接 -> outbound == true
+	//
+	// 如果我们接收并获取到一个连接 -> outbound == true
+	outbound bool
+}
+
+func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
+	return &TCPPeer{
+		conn:     conn,
+		outbound: outbound,
+	}
+}
+
 type TCPTransport struct {
 	listenAddress string
 	listener      net.Listener
